fe6b9c6f561ff29aa3974564c457016dfc8705fa
chenyff <chen_yff@126.com>
2020-11-11 18:19:06 +0800

    积跬步

--- talkpool/pages/index/index.js
+++ talkpool/pages/index/index.js
@@ -4,7 +4,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+     index:1,
+     pageIndex:0,
+     knowledge:[
+       {
+         title:'悯农',
+         content:'锄禾日当午，汗滴禾下土。谁知盘中餐，粒粒皆辛苦。',
+         remarks:'形容农民不容易'
+       },
+       {
+        title:'春晓',
+        content:'春眠不觉晓，处处闻啼鸟。夜来风雨声，花落知多少。',
+        remarks:'写得好'
+      },
+      {
+       title:'静夜思',
+       content:'床前明月光，疑似地上霜。举头望明月，低头思故乡',
+       remarks:'李白想家了'
+     }
+     ]
   },
 
   /**
@@ -20,46 +38,13 @@ Page({
   onReady: function () {
     
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  //点击上一个或者下一个
+  changeCon:function(event){
+    let nextIndex = this.data.index+event.currentTarget.dataset.number*1;
+    if(this.data.knowledge[nextIndex]){
+      this.setData({
+        index:nextIndex
+      })
+    }
   }
 })
